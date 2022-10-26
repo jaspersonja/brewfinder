3c4aa9b0139f84d1816cfd48bf57e74674ec7ffb
Ken Kaiser <kenkaiser19@gmail.com>
2022-10-25 18:46:59 -0700

    edits

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
 
+var key = "Av5Fbku8ilvcyDQyVx6HKDgfQDxZLNbIa6wW02I16qHTK6lI6nSAGuxibjOTwe32"
 var cityInput = document.querySelector("#city-search")
 fetch("https://api.openbrewerydb.org/breweries?by_city=" + cityInput + "&per_page=3")
+
+fetch('https://dev.virtualearth.net/REST/v1/LocalSearch/?query=coffee&userLocation=47.602038,-122.333964&key=' + key), {
+
+method: 'GET', //GET is the default.
+credentials: 'same-origin', // include, *same-origin, omit
+redirect: 'follow', // manual, *follow, error
+}
